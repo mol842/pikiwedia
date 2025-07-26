259706131e2c92e49d9c2b945c6739b7e818ed11
Molly <molly.stow@student.unimelb.edu.au>
2025-07-26 21:20:16 +1000

    scrombing pairs instead to create sam handwiches. also fixed links

--- functions/scrambler.js
+++ functions/scrambler.js
@@ -15,6 +15,18 @@ function scramble(text) {
     .join('')
 }
 
+const stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+function scromble_pairs(text) {
+  return text.replace(/\b(\w+)\s+(\w+)\b/g, (match, w1, w2) => {
+    if (w1.length > 3 && w2.length > 3 && !(w1 in stopwords) & !(w1 in stopwords) ) {
+      return w2[0] + w1.slice(1) + ' ' + w1[0] + w2.slice(1)
+    }
+    return match
+  })
+}
+
+
 const fetch = require('node-fetch')
 
 exports.handler = async function (event) {
@@ -39,7 +51,12 @@ exports.handler = async function (event) {
 
     // scromble all the text!!!!!!! 
     // everything between > tags <
-    html = html.replace(/>([^<]+)</g, (match, text) => `>${scramble(text)}<`)
+    html = html.replace(/>([^<]+)</g, (match, text) => `>${scromble_pairs(text)}<`)
+    // get rid of wiki links
+    // html = html.replace(/href="\/wiki\/([^"]+)"/g, 'href="pikiwedia.netlify.app/$1"')
+
+    // so the links work
+    html = html.replace(/\.\//g, (match, text) => `https://pikiwedia.netlify.app/`)
 
       // <link rel="stylesheet" href="https://en.wikipedia.org/w/load.php?modules=site.styles&only=styles">
       // <link rel="stylesheet" href="https://en.wikipedia.org/w/load.php?modules=skins.vector.styles.legacy&only=styles">
